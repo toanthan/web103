86b2d7848c845ae3208a352bf7d21eb7295d7a9f
Toan Than <toanthan.thich@gmail.com>
2019-08-20 20:16:49 +0700

    Add templates

--- complete/src/main/java/pqsoft/hrm/Employee.java
+++ complete/src/main/java/pqsoft/hrm/Employee.java
@@ -1,5 +1,7 @@
 package pqsoft.hrm;
 
+import java.util.*;
+
 public class Employee {
 
     private long id;
@@ -22,5 +24,56 @@ public class Employee {
 
     public String getFullName() {
         return String.format("%s %s", firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Employee employee = (Employee) o;
+        return
+                Objects.equals(firstName, employee.firstName) &&
+                        Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    public static void main(String[] args) {
+        Employee empl1 = new Employee(1L, "test1", "test1");
+        Employee empl2 = new Employee(2L, "test1", "test1");
+        System.out.println(empl1.equals(empl2));
+
+        List<Employee> empls = new ArrayList<>();
+        empls.add(empl1);
+        empls.add(empl2);
+
+        System.out.printf("List size: %s", empls.size());
+        System.out.printf("First item: %s%n", empls.get(1));
+
+        Set<Employee> sets = new HashSet<>();
+        sets.add(empl1);
+        sets.add(empl2);
+        System.out.printf("Set size: %s", sets.size());
+
+        final Map<String, Employee> map = new HashMap<>();
+        map.put("1", empl1);
+        map.put("2", empl1);
+        map.get("2");
     }
 }
--- complete/src/main/java/pqsoft/hrm/EmployeeController.java
+++ complete/src/main/java/pqsoft/hrm/EmployeeController.java
@@ -1,19 +1,21 @@
 package pqsoft.hrm;
 
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
 public class EmployeeController {
-
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/employees")
-    public Employee employee(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Employee(counter.incrementAndGet(),
-                            String.format(template, name), name);
+    @GetMapping("/employees")
+    public String employee(@RequestParam(value="name", defaultValue="World") String name,
+                           Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
