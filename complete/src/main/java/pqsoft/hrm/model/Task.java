b8f02a15c5ca67d2ea06bb67e389d94dcb0080ae
Dat Tran <dat.tran@sentifi.com>
2019-08-23 16:31:00 +0700

    Employee test

--- complete/src/main/java/pqsoft/hrm/model/Task.java
+++ complete/src/main/java/pqsoft/hrm/model/Task.java
@@ -1,117 +1,115 @@
 package pqsoft.hrm.model;
 
-import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.*;
 
 @Entity
 public class Task {
-	private final static int NEW = 0;
-	private final static int IN_PROGRESS = 1;
-	private final static int DONE = 2;
+  private static final int NEW = 0;
+  private static final int IN_PROGRESS = 1;
+  private static final int DONE = 2;
 
-	@Id
-	@GeneratedValue(strategy= GenerationType.AUTO)
-	private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
 
-	@Column(name = "task_name")
-	private String taskName;
+  @Column(name = "task_name")
+  private String taskName;
 
-	private String description;
+  private String description;
 
-	private int status;
-	private Date beginAt;
-	private Date finishAt;
+  private int status;
+  private Date beginAt;
+  private Date finishAt;
 
-	@Column(name = "created_at")
-	private Date createdAt;
+  @Column(name = "created_at")
+  private Date createdAt;
 
-	@Column(name = "updated_at")
-	private Date updatedAt;
+  @Column(name = "updated_at")
+  private Date updatedAt;
 
-	@OneToOne
-	private Employee creator;
+  @OneToOne private Employee creator;
 
-	@ManyToMany
-	private List<Employee> assignees;
+  @ManyToMany private List<Employee> assignees;
 
-	public int getId() {
-		return id;
-	}
+  public int getId() {
+    return id;
+  }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+  public void setId(int id) {
+    this.id = id;
+  }
 
-	public String getTaskName() {
-		return taskName;
-	}
+  public String getTaskName() {
+    return taskName;
+  }
 
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
+  public void setTaskName(String taskName) {
+    this.taskName = taskName;
+  }
 
-	public String getDescription() {
-		return description;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-	public Date getCreatedAt() {
-		return createdAt;
-	}
+  public Date getCreatedAt() {
+    return createdAt;
+  }
 
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
 
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
+  public Date getUpdatedAt() {
+    return updatedAt;
+  }
 
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
+  public void setUpdatedAt(Date updatedAt) {
+    this.updatedAt = updatedAt;
+  }
 
-	public Employee getCreator() {
-		return creator;
-	}
+  public Employee getCreator() {
+    return creator;
+  }
 
-	public void setCreator(Employee creator) {
-		this.creator = creator;
-	}
+  public void setCreator(Employee creator) {
+    this.creator = creator;
+  }
 
-	public List<Employee> getAssignees() {
-		return assignees;
-	}
+  public List<Employee> getAssignees() {
+    return assignees;
+  }
 
-	public void setAssignees(List<Employee> assignees) {
-		this.assignees = assignees;
-	}
+  public void setAssignees(List<Employee> assignees) {
+    this.assignees = assignees;
+  }
 
-	public int getStatus() {
-		return status;
-	}
+  public int getStatus() {
+    return status;
+  }
 
-	public void setStatus(int status) {
-		this.status = status;
-	}
+  public void setStatus(int status) {
+    this.status = status;
+  }
 
-	public Date getBeginAt() {
-		return beginAt;
-	}
+  public Date getBeginAt() {
+    return beginAt;
+  }
 
-	public void setBeginAt(Date beginAt) {
-		this.beginAt = beginAt;
-	}
+  public void setBeginAt(Date beginAt) {
+    this.beginAt = beginAt;
+  }
 
-	public Date getFinishAt() {
-		return finishAt;
-	}
+  public Date getFinishAt() {
+    return finishAt;
+  }
 
-	public void setFinishAt(Date finishAt) {
-		this.finishAt = finishAt;
-	}
+  public void setFinishAt(Date finishAt) {
+    this.finishAt = finishAt;
+  }
 }
